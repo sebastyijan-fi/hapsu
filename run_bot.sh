5477f084f08e7fed6dd0f7bf50b478f91c5686e8
sebastyijan-fi <westinni@arcada.fi>
2023-05-13 10:26:46 +0000

    script chnages

--- run_bot.sh
+++ run_bot.sh
@@ -5,7 +5,7 @@ echo "Starting setup..."
 # Check if the directory for the repo exists
 if [ ! -d "/hapsu" ]; then
     echo "Cloning the repo..."
-    git clone git@github.com:sebastyijan-fi/hapsu.git /hapsu || { echo "Git clone failed"; exit 1; }
+    sudo git clone git@github.com:sebastyijan-fi/hapsu.git /hapsu || { echo "Git clone failed"; exit 1; }
 fi
 
 # Navigate to the repo directory
@@ -14,7 +14,7 @@ cd /hapsu || { echo "Could not navigate to repo directory"; exit 1; }
 # Check if the .env file exists
 if [ ! -f ".env" ]; then
     # Copy env.txt to .env
-    cp env.txt .env
+    sudo cp env.txt .env
     echo "Please enter your tokens in the .env file"
 else
     # Ask the user if they want to change the .env keys
@@ -27,13 +27,13 @@ else
 fi
 
 echo "Installing Python packages..."
-pip install -r requirements.txt || { echo "Package installation failed"; exit 1; }
+sudo pip install -r requirements.txt || { echo "Package installation failed"; exit 1; }
 
 echo "Starting the bot with PM2..."
-pm2 start bot.py --name hapsu || { echo "PM2 start failed"; exit 1; }
+sudo pm2 start bot.py --name hapsu || { echo "PM2 start failed"; exit 1; }
 
 echo "Saving the PM2 process list..."
-pm2 save || { echo "PM2 save failed"; exit 1; }
+sudo pm2 save || { echo "PM2 save failed"; exit 1; }
 
 echo "Setting up cron jobs..."
 (crontab -l ; echo "0 0 * * * cd /hapsu && git pull") | crontab -
